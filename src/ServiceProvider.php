dedd0d13b323c09c68e382481dc1b59f84ec343b
Programmer <peshkov@mbober.ru>
2021-01-25 10:24:53 +0300

    initial commit

---
+++ src/ServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace MBober35\Backups;
+
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\ServiceProvider as BaseProvider;
+
+class ServiceProvider extends BaseProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        
+    }
+}
